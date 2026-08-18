f199d043b0fb9fa233ef992d8acae37144218f99
Cairo <caio12013marcelo@gmail.com>
2026-08-18 18:54:32 -0300

    Update Ex04.java

--- Henrique - PA/metodos/src/Ex04.java
+++ Henrique - PA/metodos/src/Ex04.java
@@ -1,5 +1,12 @@
 public class Ex04 {
     public static void main(String[] args) {
+        /*
+        Faça um programa que descubra:
+    a) O maior elemento de um vetor
+    b) O menor elemento de um vetor
+    c) A média dos valores
+    (Criar um método para cada questão)
+         */
         
     }
 }
