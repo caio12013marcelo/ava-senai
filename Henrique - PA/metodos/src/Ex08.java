57e0125573cb8d5a50b873aaf90e05c0ddfeda15
Cairo <caio12013marcelo@gmail.com>
2026-08-25 19:05:23 -0300

    Update Ex08.java

--- Henrique - PA/metodos/src/Ex08.java
+++ Henrique - PA/metodos/src/Ex08.java
@@ -4,18 +4,29 @@ import java.util.Scanner;
 public class Ex08 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        double somaNotas=0;
-        for (int i=0;i<=3;i++){
-            System.out.println("Informe a nota do aluno "+(i+1)+"/3");
-            double nota = input.nextInt();
-            somaNotas =  somaNotas+nota;
-        }
+
+        double totalAlunos=3;
+
+        double somaNotas=receberNotas(input,totalAlunos);
+        double mediaFinal=calcularMedia(somaNotas,totalAlunos);
+
         DecimalFormat df = new DecimalFormat(".00");
-        System.out.println("media do aluno é "+df.format(calcularMedia(somaNotas)));
+        System.out.println("media do aluno é "+df.format(mediaFinal));
         input.close();
     }
-    public static double calcularMedia(double totalNotas){
-        double media = totalNotas/3;
-        return media;
+    public static double receberNotas(Scanner input, double totalAlunos){
+        double totalNotas=0;
+
+        for (int i = 0; i < totalAlunos; i++) {
+            System.out.println("Informe um nota "+(i+1)+"/"+totalAlunos);
+            double nota= input.nextDouble();
+            totalNotas= totalAlunos+nota;
+        }
+
+        return totalNotas;
+    }
+    public static double calcularMedia(double totalNotas,double totalAlunos){
+        double mediaNotas= totalNotas/totalAlunos;
+        return mediaNotas;
     }
 }
